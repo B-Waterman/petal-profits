c76ece10f1f44466e65660f48151cc3b12a23388
Tara MacKinnon <taramackinnon4@gmail.com>
2023-05-13 15:49:40 -0700

    set up seed/migration files and route files

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,15 +1,27 @@
+// load .env data into process.env
+require('dotenv').config();
+
 const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
-const PORT = 8080;
+const PORT = process.env.DB_PORT || 8080
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
 
-// Sample GET route
-App.get('/api/data', (req, res) => res.json({
+const transactionsRoutes = require('./routes/transactions');
+const plantgoalsRoutes = require('./routes/plantgoals');
+const settingsRoutes = require('./routes/settings');
+
+App.use('/transactions', transactionsRoutes);
+App.use('/plantgoals', plantgoalsRoutes);
+App.use('/settings', settingsRoutes);
+
+
+// homepage route (example below - change when working on homepage)
+App.get('/', (req, res) => res.json({
   message: "Seems to work!",
 }));
 
