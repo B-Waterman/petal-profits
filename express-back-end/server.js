ae5984f81c19cabdf5ebbf1de9684cbcaa07d869
Tara MacKinnon <taramackinnon4@gmail.com>
2023-05-15 10:31:11 -0700

    start adding plaid api

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -6,6 +6,21 @@ const App = Express();
 const BodyParser = require('body-parser');
 const PORT = process.env.DB_PORT || 8080
 
+//Plaid configuration
+const { Configuration, PlaidApi, Products, PlaidEnvironments} = require('plaid');
+const configuration = new Configuration({
+  basePath: PlaidEnvironments['sandbox'],
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': process.env.CLIENT_ID,
+      'PLAID-SECRET': process.env.SECRET,
+      // 'Plaid-Version': '2020-09-14',
+    },
+  },
+});
+const client = new PlaidApi(configuration);
+
+
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
@@ -24,6 +39,37 @@ App.use('/settings', settingsRoutes);
 App.get('/', (req, res) => res.json({
   message: "Seems to work!",
 }));
+
+//configure test user 
+App.get('/sandbox/public_token/create', async (req, res) => {
+  const institutionID = "ins_3"
+  const initialProducts = ['balance', 'transactions']
+
+  const publicTokenRequest = {
+    institution_id: institutionID,
+    initial_products: initialProducts,
+  };
+  try {
+    const publicTokenResponse = await client.sandboxPublicTokenCreate(
+      publicTokenRequest,
+    );
+    const publicToken = publicTokenResponse.data.public_token;
+    // The generated public_token can now be exchanged
+    // for an access_token
+    const exchangeRequest = {
+      public_token: publicToken,
+    };
+    const exchangeTokenResponse = await client.itemPublicTokenExchange(
+      exchangeRequest,
+    );
+    const accessToken = exchangeTokenResponse.data.access_token;
+    console.log(accessToken)
+  } catch (error) {
+    // handle error
+    console.log(error)
+  }
+  
+})
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
