497e90371d40a2a1c15a5fc89f0226ccae1c27fc
Noidea22 <mathewsteenwinkel@gmail.com>
2023-05-15 18:24:28 -0600

    css changes to nav bar

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,75 +2,37 @@ import {useEffect} from 'react';
 import axios from 'axios';
 import './App.scss';
 import logo from './petalprofit.png';
-import { Link } from 'react-router-dom';
+import { Link, BrowserRouter} from 'react-router-dom';
 
 
 export default function App(props){
 
-  const summaryPage = () => {
-    axios.get("/")
-    .then(response => console.log(response.data))
-    .catch(error => console.log(error))
-  }
 
-  const budgetPage = () => {
-    axios.get("/transactions")
-    .then(response => console.log(response.data))
-    .catch(error => console.log(error))
-  }
-  
-  const gardenPage = () => {
-    axios.get("/garden")
-    .then(response => console.log(response.data))
-    .catch(error => console.log(error))
-  }
-  
-  const accountPage = () => {
-    axios.get("/accounts")
-    .then(response => console.log(response.data))
-    .catch(error => console.log(error))
-  } 
 
-    // return (
-    //   <div className="App">
-    //     <nav className="navBar">
-    //       <span className="logo">
-    //         <img src={logo} />
-    //       </span>
-    //       <div className="Summary">
-    //         <button onClick={summaryPage}>Overview</button>
-    //       </div>
-    //       <div className="Budget">
-    //         <button onClick={budgetPage}>My Budget</button>
-    //       </div>
-    //       <div className="Garden">
-    //         <button onClick={gardenPage}>My Garden</button>
-    //       </div>
-    //       <div className="Account">
-    //         <button onClick={accountPage}>My Account</button>
-    //       </div>
-    //     </nav>
-    //   </div>
-    // );
-
+ 
     return (
+      <BrowserRouter>
+
       <div className="App">
         <nav className="navBar">
-        <ul>
-          <li className="logo">
-            <img src={logo} />
-          </li>
-          <li className="Summary">
-            <Link to="/">Overview</Link>
-          </li>
-          <li>
+          <span className="logo">
+            <img src={logo} alt="Petal Profits" />
+          </span>
+          <div className="navButton">
+            {/* <button onClick={axios.get('/')}>Overview</button> */}
+              <Link to="/">Overview</Link>
+          </div>
+          <div className="navButton">
             <Link to="/transactions">My Budget</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-      </ul>
+          </div>
+          <div className="navButton">
+            <Link to="/garden">My Garden</Link>
+          </div>
+          <div className="navButton">
+            <Link to="/account">My Account</Link>
+          </div>
         </nav>
       </div>
+      </BrowserRouter>
     )
-}
+};
