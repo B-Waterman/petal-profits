704096ac95c1af8e255e74370742c7a6b8ba0544
Tara MacKinnon <taramackinnon4@gmail.com>
2023-05-16 12:58:15 -0700

    capture data and put into state

--- react-front-end/src/components/Budget.js
+++ react-front-end/src/components/Budget.js
@@ -1,6 +1,24 @@
 import './Budget.scss';
+import React, { useEffect, useState} from 'react';
+import axios from 'axios'
 
 export default function Budget(props) {
+  
+  const [state, setState] = useState({
+    transactions: [],
+    accounts: []
+  })
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/transactions')
+      .then(response => {
+        console.log(response.data)
+        setState(prev => ({ ...prev, transactions: response.data.transactions }));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
