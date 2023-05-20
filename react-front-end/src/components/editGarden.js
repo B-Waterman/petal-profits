2bf0005603d1450018bb2c908c6ec16aba86d2b3
Noidea22 <mathewsteenwinkel@gmail.com>
2023-05-20 14:23:39 -0600

    working on modal

--- react-front-end/src/components/editGarden.js
+++ react-front-end/src/components/editGarden.js
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 
 const EditGarden = ({ open, onClose, goals }) => {
   const [name, newName] = useState(goals[0].name);
+  const [target, newTarget] = useState(goals[0].target_amount)
 console.log(goals[0].name)
 
   return (
     <>
       <form className="overlay">
-        <div className="modalContainer">
+        <div className="modalContainer" id={`id$()`}>
           <h1>edit</h1>
           <p>edit the name of the goal</p>
           <input type='text' value={name}></input>
           <p>change the goal amount</p>
-          <input type='number'></input>
+          <input type='number' value={target}></input>
 
           <button onClick={onClose} className='closeBtn'>X</button>
         </div>
