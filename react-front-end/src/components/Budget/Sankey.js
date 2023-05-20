1cb23246ef3b3494220c808c338f39a5f9abc97b
Tara MacKinnon <taramackinnon4@gmail.com>
2023-05-20 12:13:00 -0700

    removed bootstrap and updated styling for sankey

--- react-front-end/src/components/Budget/Sankey.js
+++ react-front-end/src/components/Budget/Sankey.js
@@ -7,44 +7,44 @@ export default function Sankey() {
     console.log(GetSankeyData())
     
       return(
-        <ResponsiveSankey 
-        data={data}
-            margin={{ top: 40, right: 250, bottom: 40, left: 250 }}
-            valueFormat=" <-$1,.0f"
-            sort='auto'
-            colors={{ scheme: 'category10' }}
-            nodeOpacity={0.75}
-            nodeHoverOthersOpacity={0.5}
-            nodeThickness={8}
-            nodeSpacing={40}
-            nodeBorderWidth={0}
-            nodeBorderColor={{
-                from: 'color',
-                modifiers: [
-                    [
-                        'darker',
-                        0.8
+            <ResponsiveSankey 
+            data={data}
+                margin={{ top: 20, right: 250, bottom: 20, left: 180 }}
+                valueFormat=" <-$1,.0f"
+                sort='auto'
+                colors={{ scheme: 'category10' }}
+                nodeOpacity={0.75}
+                nodeHoverOthersOpacity={0.5}
+                nodeThickness={8}
+                nodeSpacing={40}
+                nodeBorderWidth={0}
+                nodeBorderColor={{
+                    from: 'color',
+                    modifiers: [
+                        [
+                            'darker',
+                            0.8
+                        ]
                     ]
-                ]
-            }}
-            nodeBorderRadius={2}
-            linkOpacity={0.35}
-            linkHoverOthersOpacity={0.25}
-            linkContract={2}
-            enableLinkGradient={true}
-            enableLabels={true}
-            labelPosition="outside"
-            labelPadding={27}
-            labelTextColor={{
-                from: 'color',
-                modifiers: [
-                    [
-                        'darker',
-                        '1'
+                }}
+                nodeBorderRadius={2}
+                linkOpacity={0.35}
+                linkHoverOthersOpacity={0.25}
+                linkContract={2}
+                enableLinkGradient={true}
+                enableLabels={true}
+                labelPosition="outside"
+                labelPadding={27}
+                labelTextColor={{
+                    from: 'color',
+                    modifiers: [
+                        [
+                            'darker',
+                            '1'
+                        ]
                     ]
-                ]
-            }}
-        />
+                }}
+            />
     )
 
 }
