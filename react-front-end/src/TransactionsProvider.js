ec41a088b0045e62ce9c2b23dca27d4ad6175bd7
B-Waterman <brooke.waterman@outlook.com>
2023-05-22 11:50:07 -0700

    Implement Donut chart for expenses view

--- react-front-end/src/TransactionsProvider.js
+++ react-front-end/src/TransactionsProvider.js
@@ -10,19 +10,33 @@ export default function TransactionsProvider(props) {
 
   // Here is our Shared State Object
   const [transactions, setTransactions] = useState([]);
+  const [accounts, getAccounts] = useState([]);
+
+  const [loading, setLoading] = useState(true);
 
   // Get transaction/category info and set state item
   useEffect(() => {
-    axios.get('http://localhost:8080/api/transactions')
-      .then((response) => {
-        setTransactions(response.data);
+    Promise.all([
+      axios.get('http://localhost:8080/api/transactions'),
+      axios.get('http://localhost:8080/api/accounts')
+    ])
+      .then((all) => {
+        setTransactions(all[0].data);
+        getAccounts(all[1].data);
+        setLoading(false)
       })
       .catch(error => {
         console.log(error);
+        setLoading(false)
       });
   }, []);
 
-  const data = { transactions };
+  const data = { transactions, accounts };
+
+  if (loading) {
+    // You can return a loading indicator here
+    return <p>Loading...</p>;
+  }
 
   // We can now use this as a component to wrap anything
   // that needs our state
@@ -32,3 +46,4 @@ export default function TransactionsProvider(props) {
     </transactionsContext.Provider>
   );
 };
+
---
+++ react-front-end/src/components/DonutChart.js
@@ -0,0 +1,104 @@
+import React, { useContext } from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { categoriesContext } from "./Budget/providers/CategoriesProvider";
+import { formatTitle } from './Budget/helpers/formatTitle'
+import {Chart, ArcElement, Tooltip, Legend} from 'chart.js'
+Chart.register(ArcElement, Tooltip, Legend);
+
+export default function DonutChart() {
+
+  const { categories } = useContext(categoriesContext);
+
+  const catLabels = [] //hold name of categories
+  const catData = [] //hold total amount in each category
+
+  categories.forEach(obj => {
+      if (obj.name !== "INCOME") {
+        const title = formatTitle(obj.name)
+        const value = obj.sum
+        catLabels.push(title)
+        catData.push(value)
+    }
+  });
+
+  const data = {
+    labels: catLabels,
+      datasets: [
+        {
+          label: 'Expenses',
+          data: catData,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1,
+        },
+      ],
+
+  };
+
+  const options = {
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: function(context) {
+            let label = context.dataset.label || '';
+
+            if (label) {
+                label += ': ';
+            }
+            if (context.parsed.y !== null) {
+                label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.raw);
+            }
+            return label;
+          },
+        },
+      },
+    },
+  };
+
+//   // const textCenter = {
+//   //   id: 'textCenter',
+//   //   beforeDatasetsDraw(chart, args, pluginOptions) {
+//   //     const { ctx, data } = chart;
+//   //     ctx.save();
+//   //     ctx.font = 'bolder 50px sans-serif';
+//   //     ctx.fillStyle ='red';
+//   //     ctx.textAlign = 'center';
+//   //     ctx.textBaseline = 'middle';
+//   //     ctx.fillText('Expenses', chart.getDatasetMeta(0).data[0].x, chart.getDatasetMeta(0).data[0].y);
+//   //   }
+//   // }
+
+
+
+  return (
+    <Doughnut data={data} options={options}/> //plugins ={textCenter} />
+  )
+
+};
+
